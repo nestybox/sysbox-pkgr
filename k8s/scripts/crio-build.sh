0d9d997dede4bcff39751b087b3d97d5ad07c30a
Cesar Talledo <ctalledo@nestybox.com>
2022-05-18 04:30:52 +0000

    Build CRI-O inside a container.
    Prior to this change, the sysbox-deploy-k8s Makefile was building the
    customized CRI-O at host level. This caused problems as the build
    was not working properly on some hosts.
    
    This change builds CRI-O inside a container for more consistency.
    
    Signed-off-by: Cesar Talledo <ctalledo@nestybox.com>

---
+++ k8s/scripts/crio-build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#
+# CRI-O build script (meant to run inside the CRI-O build container)
+#
+# The script will build the Nestybox-customized CRI-O and place the binaries
+# under /mnt/results. It builds several versions of CRI-O.
+#
+# Usage: docker run -v $(shell pwd)/bin:/mnt/results crio-bld
+#
+
+declare -a CRIO_VERSIONS=(v1.20 v1.21 v1.22 v1.23)
+
+for ver in ${CRIO_VERSIONS[@]}; do
+	printf "\n*** Building CRI-O ${ver} ... ***\n\n"
+	TMPDIR=$(mktemp -d)
+	chmod 755 ${TMPDIR}
+	git clone https://github.com/nestybox/cri-o.git ${TMPDIR}/cri-o
+	git -C ${TMPDIR}/cri-o checkout -b ${ver}-sysbox origin/${ver}-sysbox
+	cd ${TMPDIR}/cri-o && make binaries
+	mkdir -p /mnt/results/bin/crio/${ver}
+	cp ${TMPDIR}/cri-o/bin/crio-static /mnt/results/crio/${ver}/crio
+done
