e7560689ee8cc9e3bec38cc6543761767b1087c9
Rodny Molina <rodny.molina@docker.com>
2023-01-26 01:57:23 +0000

    [k8s] Extend support to k8s/crio v1.26
    Signed-off-by: Rodny Molina <rodny.molina@docker.com>

--- k8s/scripts/sysbox-deploy-k8s.sh
+++ k8s/scripts/sysbox-deploy-k8s.sh
@@ -744,12 +744,18 @@ function is_supported_k8s_version() {
 
 	local ver=$k8s_version
 
-	if [[ "$ver" == "v1.21" ]] ||
-		[[ "$ver" == "v1.22" ]] ||
+	if [[ "$ver" == "v1.22" ]] ||
 		[[ "$ver" == "v1.23" ]] ||
 		[[ "$ver" == "v1.24" ]] ||
-		[[ "$ver" == "v1.25" ]]; then
+		[[ "$ver" == "v1.25" ]] ||
+		[[ "$ver" == "v1.26" ]]; then
 		return
+	fi
+
+	if [[ "$ver" == "v1.19" ]] ||
+		[[ "$ver" == "v1.20" ]] ||
+		[[ "$ver" == "v1.21" ]]; then
+		echo "Unsupported kubernetes version: $ver (EOL release)."
 	fi
 
 	false
